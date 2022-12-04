de85cb78e1fffd5dc7b7affcc0c21976bcc6c8cd
Cside <cside.story@gmail.com>
2022-12-04 18:02:52 +0900

    postcss

--- src/ts/main.tsx
+++ src/ts/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import '../postcss/style.pcss';
 
-window.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementsByClassName('js-root')[0]).render(
-    <div>Hello</div>,
-  );
-});
+createRoot(document.getElementsByClassName('js-root')[0]).render(
+  <div>Hello</div>,
+);
